2089a31edcef849e0634431580423f37aca0cad5
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2018-11-24 19:51:20 +0100

    sketch added to the project
    It uses Adafruit BME280 library to measure temperature, humidity, and
    pressure. The sensor is supposed to be connected via i2c.
    
    The program is interrogated by remote call via serial port communication.

---
+++ arduino/sketch.ino
@@ -0,0 +1,40 @@
+// vim: filetype=cpp
+
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme;
+
+auto constexpr PID = 0x0EEE;
+auto constexpr speed = 9600;
+
+void setup() {
+  Serial.begin(speed);
+  auto const status = bme.begin();
+
+  if (status == 0) {
+    Serial.println("BME error");
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    auto const order = Serial.read();
+
+    if (order == 't') {
+      Serial.print("T = "); Serial.println(bme.readTemperature());
+    } else if (order == 'h') {
+      Serial.print("H = "); Serial.println(bme.readHumidity());
+    } else if (order == 'p') {
+      Serial.print("P = "); Serial.println(bme.readPressure());
+    } else if (order == 'e') {
+      Serial.println(PID, HEX);
+    } else {
+      Serial.println(
+          "Usage:\n"                 \
+          " - 't' for temperature\n" \
+          " - 'h' for humidity\n"    \
+          " - 'p' for pressure"
+      );
+    }
+  }
+}
