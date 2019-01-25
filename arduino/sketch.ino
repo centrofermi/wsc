da561b91b28949bd8ecedc043630460d47454a09
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2019-01-25 16:57:32 +0100

    external-temperature sensor integrated
    the command for it is 'o'.

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -1,8 +1,11 @@
 // vim: filetype=cpp
 
 #include <Adafruit_BME280.h>
+#include <DallasTemperature.h>
 
 Adafruit_BME280 bme;
+OneWire owds(6);
+DallasTemperature ds(&owds);
 
 auto constexpr PID = 0x0EEE;
 auto constexpr speed = 9600;
@@ -10,9 +13,16 @@ auto constexpr speed = 9600;
 void setup() {
   Serial.begin(speed);
   auto const bme_status = bme.begin();
+  ds.begin();
 
   if (bme_status == 0) {
     Serial.println("BME error");
+  }
+
+  auto const ds_count = ds.getDeviceCount();
+
+  if (ds_count == 0) {
+    Serial.println("DS18B20 error");
   }
 }
 
@@ -28,10 +38,14 @@ void loop() {
       Serial.print("P = "); Serial.println(bme.readPressure());
     } else if (order == 'e') {
       Serial.println(PID, HEX);
+    } else if (order == 'o') {
+      ds.requestTemperaturesByIndex(0);
+      Serial.print("O = "); Serial.println(ds.getTempCByIndex(0));
     } else {
       Serial.println(
           "Usage:\n"                 \
           " - 't' for inside temperature\n" \
+          " - 'o' for outside temperature\n" \
           " - 'h' for humidity\n"    \
           " - 'p' for pressure"
       );
